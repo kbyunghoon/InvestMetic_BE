2caae0776555dbee9c3cc98e1e0f105177a4862c
kbyunghoon <dev.kbhoon@gmail.com>
2024-11-06 18:19:16 +0900

    Fix BaseResponse

--- src/main/java/com/investmetic/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/investmetic/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,4 @@
 package com.investmetic.global.exception;
-
-<<<<<<< Updated upstream
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +10,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-
-
 /**
  * 애플리케이션 전역 예외를 처리
  */
@@ -32,7 +28,6 @@ public class GlobalExceptionHandler {
                 .status(e.getBaseResponseCode().getStatus().value())
                 .body(new BaseResponse<>(e.getBaseResponseCode()));
     }
-
     /**
      * enum 타입이 일치하지 않을 때 발생하는 예외를 처리
      * @RequestParam으로 전달된 enum 타입의 값이 맞지 않을 때 주로 발생
@@ -46,8 +41,6 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST.value())
                 .body(new BaseResponse<>(BaseResponseCode.INVALID_DATE));
     }
-
-
     /**
      * @Valid 또는 @Validated로 유효성 검사 실패 시 발생하는 예외를 처리
      * HttpMessageConverter에서 유효하지 않은 데이터를 변환할 때 발생
@@ -63,12 +56,10 @@ public class GlobalExceptionHandler {
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
             builder.append(fieldError.getDefaultMessage());
         }
-
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST.value())
                 .body(new BaseResponse<>(HttpStatus.BAD_REQUEST.name(), builder.toString()));
     }
-
     /**
      * 요청에 필수적인 Path Variable이 없을 때 발생하는 예외를 처리
      *
@@ -81,7 +72,6 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST.value())
                 .body(new BaseResponse<>(BaseResponseCode.EMPTY_PATH_VARIABLE));
     }
-
     /**
      * 지원되지 않는 HTTP 메서드로 요청할 때 발생하는 예외를 처리
      *
@@ -93,84 +83,5 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST.value())
                 .body(new BaseResponse<>(BaseResponseCode.METHOD_NOT_ALLOWED));
-=======
-import com.investmetic.global.exception.error.ErrorCode;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
-@Slf4j
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
-    /**
-     * javax.validation.Valid or @Validated 바인딩 오류 시 발생.
-     * 주로 @RequestBody, @RequestPart에서 발생
-     */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        log.error("handleMethodArgumentNotValidException", e);
-        final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    /**
-     * enum 타입 일치하지 않아 바인딩 못할 경우 발생.
-     * 주로 @RequestParam enum으로 바인딩하지 못했을 경우 발생
-     */
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
-            MethodArgumentTypeMismatchException e) {
-        log.error("handleMethodArgumentTypeMismatchException", e);
-        final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_TYPE_VALUE);
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    /**
-     * 비즈니스 로직 예외 처리
-     */
-    @ExceptionHandler(BusinessException.class)
-    protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
-        log.error("handleBusinessException", e);
-        final ErrorCode errorCode = e.getErrorCode();
-        final ErrorResponse response = ErrorResponse.of(errorCode);
-        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
-    }
-
-    /**
-     * 존재하지 않는 URL에 대한 요청 처리
-     */
-    @ExceptionHandler(NoHandlerFoundException.class)
-    protected ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException e) {
-        log.error("handleNoHandlerFoundException", e);
-        final ErrorResponse response = ErrorResponse.of(ErrorCode.ENTITY_NOT_FOUND);
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-
-    /**
-     * HTTP 메서드가 지원되지 않는 경우 처리
-     */
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e) {
-        log.error("handleHttpRequestMethodNotSupported", e);
-        final ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
-        return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
-    }
-
-    /**
-     * 기타 모든 예외에 대한 처리
-     */
-    @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error("handleException", e);
-        final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
->>>>>>> Stashed changes
     }
 }
