f407f73207de644e8a2031d9fa0e26bd4cfd57a3
kbyunghoon <dev.kbhoon@gmail.com>
2024-11-09 19:12:42 +0900

    🐛 [fix] 롤백

--- src/main/java/com/investmetic/global/exception/BaseResponse.java
+++ src/main/java/com/investmetic/global/exception/BaseResponse.java
@@ -2,7 +2,6 @@ package com.investmetic.global.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
-import org.springframework.http.ResponseEntity;
 
 /**
  * BaseResponse는 API 요청에 대한 응답 표준화 클래스
@@ -10,8 +9,8 @@ import org.springframework.http.ResponseEntity;
  * @param <T> 요청의 결과 데이터 타입
  */
 @Getter
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BaseResponse<T> {
+
     private final Boolean isSuccess; // 상태 코드에 따른 Boolean
     private final String message;    // 에러 설명 또는 성공 메시지
 
@@ -22,41 +21,28 @@ public class BaseResponse<T> {
     private final Integer code;      // 성공 또는 오류 코드
 
     // 생성자
-    private BaseResponse(Boolean isSuccess, String message, T result, Integer code) {
+    private BaseResponse(Boolean isSuccess, Integer code, String message, T result) {
         this.isSuccess = isSuccess;
+        this.code = code;
         this.message = message;
         this.result = result;
-        this.code = code;
     }
 
-    // 성공 응답 생성 메서드
-    public static <T> ResponseEntity<BaseResponse<T>> success(SuccessCode code, T data) {
-        return ResponseEntity
-                .status(code.getStatus())
-                .body(new BaseResponse<>(true, code.getMessage(), data, null));
+    // 성공 응답 생성자
+    public static <T> BaseResponse<T> success() {
+        return new BaseResponse<>(true, null, SuccessCode.OK.getMessage(), null);
     }
 
-    public static <T> ResponseEntity<BaseResponse<T>> success(T data) {
-        return ResponseEntity
-                .status(SuccessCode.OK.getStatus())
-                .body(new BaseResponse<>(true, SuccessCode.OK.getMessage(), data, null));
+    public static <T> BaseResponse<T> success(T data) {
+        return new BaseResponse<>(true, null, SuccessCode.OK.getMessage(), data);
     }
 
-    public static <T> ResponseEntity<BaseResponse<T>> success(SuccessCode code) {
-        return ResponseEntity
-                .status(code.getStatus())
-                .body(new BaseResponse<>(true, code.getMessage(), null, null));
-    }
-
-    // 성공 응답 생성 메서드
-    public static <T> ResponseEntity<BaseResponse<T>> success() {
-        return ResponseEntity
-                .status(SuccessCode.OK.getStatus())
-                .body(new BaseResponse<>(true, SuccessCode.OK.getMessage(), null, null));
+    public static <T> BaseResponse<T> success(SuccessCode code, T data) {
+        return new BaseResponse<>(true, null, code.getMessage(), data); // 성공 시 code는 null로 설정
     }
 
     // 실패 응답 생성자
     public static <T> BaseResponse<T> fail(ErrorCode code) {
-        return new BaseResponse<>(false, code.getMessage(), null, code.getStatusCode());
+        return new BaseResponse<>(false, code.getStatusCode(), code.getMessage(), null);
     }
 }
--- src/main/java/com/investmetic/global/exception/SuccessCode.java
+++ src/main/java/com/investmetic/global/exception/SuccessCode.java
@@ -8,14 +8,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
-    OK(HttpStatus.OK, 200, "요청이 성공했습니다."),
-    CREATED(HttpStatus.CREATED, 201, "자원이 성공적으로 생성되었습니다."),
-    UPDATED(HttpStatus.OK, 202, "자원이 성공적으로 업데이트되었습니다."),
-    DELETED(HttpStatus.OK, 203, "자원이 성공적으로 삭제되었습니다.");
+    OK(HttpStatus.OK, "요청이 성공했습니다."),
+    CREATED(HttpStatus.CREATED, "자원이 성공적으로 생성되었습니다."),
+    UPDATED(HttpStatus.OK, "자원이 성공적으로 업데이트되었습니다."),
+    DELETED(HttpStatus.OK, "자원이 성공적으로 삭제되었습니다.");
 
-    private final HttpStatus status;
-    private final int code;
-    private final String message;
+    private final HttpStatus status; // HTTP 상태 코드
+    private final String message; // 오류 메시지
 
     public int getStatusCode() {
         return status.value();
