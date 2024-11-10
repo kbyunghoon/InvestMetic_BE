e6b297aed56d934decb94156d42b6dfa2c41187a
5jeong <wjdgns4385@naver.com>
2024-11-10 17:30:12 +0900

    ✨ [feat] 소수점 반올림 유틸클래스 작성

---
+++ src/main/java/com/investmetic/global/util/RoundUtil.java
@@ -0,0 +1,31 @@
+package com.investmetic.global.util;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * 소수점 반올림 하는 유틸 클래스
+ */
+public class RoundUtil {
+
+    // 소수점 첫째 자리에서 반올림
+    public static double roundToFirst(double value) {
+        return round(value, 0);
+    }
+
+    // 소수점 둘째 자리에서 반올림
+    public static double roundToSecond(double value) {
+        return round(value, 1);
+    }
+
+    // 소수점 셋째 자리에서 반올림
+    public static double roundToThird(double value) {
+        return round(value, 2);
+    }
+
+    private static double round(double value, int idx) {
+        BigDecimal bigDecimal = BigDecimal.valueOf(value);
+        return bigDecimal.setScale(idx, RoundingMode.HALF_UP).doubleValue();
+    }
+
+}
