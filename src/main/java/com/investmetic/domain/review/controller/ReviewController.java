42c78567906f0c4877e5be49403bcbbd2f2af4db
5jeong <wjdgns4385@naver.com>
2024-11-10 18:06:04 +0900

    ♻️ [refactor] 엔드포인트 중복경로 설정

--- src/main/java/com/investmetic/domain/review/controller/ReviewController.java
+++ src/main/java/com/investmetic/domain/review/controller/ReviewController.java
@@ -4,11 +4,9 @@ import com.investmetic.domain.review.dto.request.ReviewRequestDto;
 import com.investmetic.domain.review.dto.response.ReviewListResponse;
 import com.investmetic.domain.review.dto.response.ReviewResponse;
 import com.investmetic.domain.review.service.ReviewService;
-import com.investmetic.global.common.PageResponseDto;
 import com.investmetic.global.exception.BaseResponse;
 import com.investmetic.global.exception.SuccessCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
@@ -18,18 +16,20 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO : 스프링시큐리티 적용완료되면  @AuthenticationPrincipal로 userId수정
 @RestController
+@RequestMapping("/api/strategies/{strategyId}/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewService reviewService;
 
     // 리뷰 등록
-    @PostMapping("/api/strategies/{strategyId}/reviews")
+    @PostMapping
     public BaseResponse<ReviewResponse> addReview(
             @PathVariable Long strategyId,
             @RequestParam Long userId, // 임시로 userId를 쿼리 파라미터로 받음
@@ -40,7 +40,7 @@ public class ReviewController {
     }
 
     // 리뷰 수정
-    @PatchMapping("/api/strategies/{strategyId}/reviews/{reviewId}")
+    @PatchMapping("/{reviewId}")
     public BaseResponse<ReviewResponse> updateReview(
             @PathVariable Long strategyId,
             @PathVariable Long reviewId,
@@ -51,7 +51,7 @@ public class ReviewController {
     }
 
     // 리뷰 삭제
-    @DeleteMapping("/api/strategies/{strategyId}/reviews/{reviewId}")
+    @DeleteMapping("/{reviewId}")
     public BaseResponse<ReviewResponse> deleteReview(
             @PathVariable Long strategyId,
             @PathVariable Long reviewId) {
@@ -61,13 +61,13 @@ public class ReviewController {
     }
 
     // 리뷰 목록 조회
-    @GetMapping("/api/strategies/{strategyId}/reviews")
+    @GetMapping
     public BaseResponse<ReviewListResponse> getReviews(
             @PathVariable Long strategyId,
             @RequestParam Long userId, // 임시로 userId를 쿼리 파라미터로 받음
-            @PageableDefault(size=5,sort="createdAt",direction = Direction.DESC) Pageable pageable){
+            @PageableDefault(size = 5, sort = "createdAt", direction = Direction.DESC) Pageable pageable) {
 
-        ReviewListResponse result = reviewService.getReviewList(strategyId,userId,pageable);
+        ReviewListResponse result = reviewService.getReviewList(strategyId, userId, pageable);
         return BaseResponse.success(result);
     }
 }
