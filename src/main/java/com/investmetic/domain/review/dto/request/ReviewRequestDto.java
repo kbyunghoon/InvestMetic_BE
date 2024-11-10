c30816b731180064743af5fc9f70f6af94b666d3
5jeong <wjdgns4385@naver.com>
2024-11-10 19:42:24 +0900

    ♻️ [refactor] 타입 변경

--- src/main/java/com/investmetic/domain/review/dto/request/ReviewRequestDto.java
+++ src/main/java/com/investmetic/domain/review/dto/request/ReviewRequestDto.java
@@ -5,9 +5,7 @@ import com.investmetic.domain.strategy.model.entity.Strategy;
 import com.investmetic.domain.user.model.entity.User;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -19,16 +17,16 @@ public class ReviewRequestDto {
     @NotNull
     @Min(value = 1)
     @Max(value = 5)
-    private Integer starRating;
+    private int starRating;
 
 
     @Builder
-    public ReviewRequestDto(String content, Integer starRating) {
+    public ReviewRequestDto(String content, int starRating) {
         this.content = content;
         this.starRating = starRating;
     }
 
-    public Review toEntity(User user, Strategy strategy){
-        return Review.createReview(strategy,user,content,starRating);
+    public Review toEntity(User user, Strategy strategy) {
+        return Review.createReview(strategy, user, content, starRating);
     }
 }
