0cbeb383a056d1cd38a712ca5eab6781111f4044
5jeong <wjdgns4385@naver.com>
2024-11-10 18:03:28 +0900

    ♻️ [refactor] 주석추가

--- src/main/java/com/investmetic/domain/strategy/model/entity/TradeType.java
+++ src/main/java/com/investmetic/domain/strategy/model/entity/TradeType.java
@@ -20,6 +20,7 @@ public class TradeType extends BaseEntity {
     @Column(length = 1000)
     private String tradeIconPath;
 
+    // 매매유형 임시용 생성자입니다. 충돌시 아래 생성코드는 삭제해주시고, 작성하신것으로 사용해주세요 -오정훈-
     @Builder
     public TradeType(Long tradeTypeId, String tradeName, boolean activate_state, String tradeIconPath) {
         this.tradeTypeId = tradeTypeId;
