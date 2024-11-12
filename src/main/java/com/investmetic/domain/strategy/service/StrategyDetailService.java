c1a9efd7ddd865c7b20b22e25f492107f5970ef3
5jeong <wjdgns4385@naver.com>
2024-11-12 17:09:55 +0900

    ✨ [feat] 트랜잭션 추가

--- src/main/java/com/investmetic/domain/strategy/service/StrategyDetailService.java
+++ src/main/java/com/investmetic/domain/strategy/service/StrategyDetailService.java
@@ -14,9 +14,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class StrategyDetailService {
     private final StrategyStatisticsRepository strategyStatisticsRepository;
     private final DailyAnalysisRepository dailyAnalysisRepository;
