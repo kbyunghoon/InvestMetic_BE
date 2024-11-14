958aff13916c326b7c4eac20cdecc9b54593b727
woong <153367605+kendrickhama@users.noreply.github.com>
2024-11-14 16:37:59 +0900

    🐛 [fix] 전략 등록 GET 요청 테스트 코드 문제 수정

--- src/test/java/com/investmetic/domain/strategy/service/StrategyServiceTest.java
+++ src/test/java/com/investmetic/domain/strategy/service/StrategyServiceTest.java
@@ -57,7 +57,8 @@ class StrategyServiceTest {
         when(tradeTypeRepository.findByActivateStateTrue()).thenReturn(tradeTypeList);
         when(stockTypeRepository.findAll()).thenReturn(stockTypeList);
 
-        ResponseEntity<BaseResponse<RegisterInfoResponseDto>> response = strategyService.loadStrategyRegistrationInfo();
+        ResponseEntity<BaseResponse<RegisterInfoResponseDto>> response = BaseResponse.success(
+                strategyService.loadStrategyRegistrationInfo());
 
         assertThat(response).isNotNull();
         assertThat(Objects.requireNonNull(response.getBody()).getIsSuccess()).isTrue();
