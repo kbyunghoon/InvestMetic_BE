032349ec3fa39a40a06219636918cdee30295077
5jeong <wjdgns4385@naver.com>
2024-11-18 21:14:52 +0900

    ✨ [feat] 전략랭킹 항목별 상세검색 구현

--- src/main/java/com/investmetic/domain/strategy/repository/StrategyRepositoryCustomImpl.java
+++ src/main/java/com/investmetic/domain/strategy/repository/StrategyRepositoryCustomImpl.java
@@ -1,23 +1,44 @@
 
 package com.investmetic.domain.strategy.repository;
 
+import static com.investmetic.domain.strategy.model.entity.QDailyAnalysis.dailyAnalysis;
 import static com.investmetic.domain.strategy.model.entity.QStockType.stockType;
 import static com.investmetic.domain.strategy.model.entity.QStockTypeGroup.stockTypeGroup;
 import static com.investmetic.domain.strategy.model.entity.QStrategy.strategy;
 import static com.investmetic.domain.strategy.model.entity.QStrategyStatistics.strategyStatistics;
 import static com.investmetic.domain.strategy.model.entity.QTradeType.tradeType;
+import static com.investmetic.domain.subscription.model.entity.QSubscription.subscription;
 import static com.investmetic.domain.user.model.entity.QUser.user;
 
+import com.investmetic.domain.strategy.dto.RangeDto;
+import com.investmetic.domain.strategy.dto.request.FilterSearchRequest;
 import com.investmetic.domain.strategy.dto.response.QStrategyDetailResponse;
 import com.investmetic.domain.strategy.dto.response.StrategyDetailResponse;
+import com.investmetic.domain.strategy.dto.response.common.QStrategySimpleResponse;
+import com.investmetic.domain.strategy.dto.response.common.StrategySimpleResponse;
+import com.investmetic.domain.strategy.model.DurationRange;
+import com.investmetic.domain.strategy.model.IsApproved;
+import com.investmetic.domain.strategy.model.IsPublic;
+import com.investmetic.domain.strategy.model.OperationCycle;
+import com.investmetic.domain.strategy.model.ProfitRange;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.StringPath;
+import com.querydsl.core.types.dsl.Wildcard;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 @RequiredArgsConstructor
 public class StrategyRepositoryCustomImpl implements StrategyRepositoryCustom {
@@ -27,18 +48,13 @@ public class StrategyRepositoryCustomImpl implements StrategyRepositoryCustom {
     @Override
     public StrategyDetailResponse findStrategyDetail(Long strategyId) {
 
-        List<Tuple> stockTypes = queryFactory
-                .select(stockType.stockTypeIconURL, stockType.stockTypeName)
-                .from(stockTypeGroup)
-                .join(stockTypeGroup.stockType, stockType)
-                .where(stockTypeGroup.strategy.strategyId.eq(strategyId))
-                .fetch();
+        List<Tuple> stockTypeInfos = getStockTypeInfos(strategyId);
 
         // 종목 아이콘 목록
-        List<String> stockTypeIconURLs = getStockTypeIconURLs(stockTypes, stockType.stockTypeIconURL);
+        List<String> stockTypeIconURLs = getStockTypeIconURLs(stockTypeInfos, stockType.stockTypeIconURL);
 
         // 종목 이름목록
-        List<String> stockTypeNames = getStockTypeIconURLs(stockTypes, stockType.stockTypeName);
+        List<String> stockTypeNames = getStockTypeIconURLs(stockTypeInfos, stockType.stockTypeName);
 
         return queryFactory
                 .select(new QStrategyDetailResponse(
@@ -71,10 +87,205 @@ public class StrategyRepositoryCustomImpl implements StrategyRepositoryCustom {
                 .fetchOne();
     }
 
+    private List<Tuple> getStockTypeInfos(Long strategyId) {
+        return queryFactory
+                .select(stockType.stockTypeIconURL, stockType.stockTypeName)
+                .from(stockTypeGroup)
+                .join(stockTypeGroup.stockType, stockType)
+                .where(stockTypeGroup.strategy.strategyId.eq(strategyId))
+                .fetch();
+    }
+
     private @NotNull List<String> getStockTypeIconURLs(List<Tuple> stockTypes, StringPath stockType) {
         return stockTypes.stream()
                 .map(tuple -> tuple.get(stockType))
                 .toList();
+    }
+
+    /***
+     * 항목별 검색 조회 쿼리(운용방식, 운용주기, 운용종목, 기간, 수익률, 원금, MDD, SM Score로 검색)
+     * 각 항목들은 중복 체크가 가능
+     * 공개중인 전략과 승인완료된 전략만 조회가능
+     * 수익률로 정렬
+     */
+    @Override
+    public Page<StrategySimpleResponse> searchByFilters(FilterSearchRequest filterSearchRequest, Long userId,
+                                                        Pageable pageable) {
+
+        List<StrategySimpleResponse> content = queryFactory
+                .select(new QStrategySimpleResponse(
+                        strategy.strategyId,
+                        strategy.strategyName,
+                        user.imageUrl,
+                        user.nickname,
+                        tradeType.tradeTypeIconURL,
+                        strategyStatistics.maxDrawdown,
+                        strategyStatistics.smScore,
+                        strategyStatistics.cumulativeProfitRate,
+                        strategyStatistics.averageProfitLossRate,
+                        strategy.subscriptionCount,
+                        strategy.averageRating,
+                        strategy.reviewCount
+                ))
+                .from(strategy)
+                .join(strategy.strategyStatistics, strategyStatistics)
+                .join(strategy.tradeType, tradeType)
+                .join(strategy.user, user)
+                .where(strategy.isApproved.eq(IsApproved.APPROVED), strategy.isPublic.eq(IsPublic.PUBLIC))
+                .where(applyAllFilters(filterSearchRequest))
+                .orderBy(strategyStatistics.cumulativeProfitRate.desc()) // 누적수익률으로 정렬
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 페이징 count 쿼리 최적화
+        JPAQuery<Long> countQuery = queryFactory
+                .select(Wildcard.count)
+                .from(strategy)
+                .where(strategy.isApproved.eq(IsApproved.APPROVED), strategy.isPublic.eq(IsPublic.PUBLIC))
+                .where(applyAllFilters(filterSearchRequest));
+
+        // 만약 페이지의 처음이나, 끝일때, 전체 데이터 크기가 pageSize보다 작은 경우 COUNT 쿼리가 실행되지 않음
+        // 그 외 경우에만 fetchOne() 을 실행하여 전체 데이터 개수를 계산
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+    }
+
+    // 수익률 그래프 데이터 조회
+    @Override
+    public Map<Long, List<Tuple>> findProfitRateDataMap(List<Long> strategyIdS) {
+        return queryFactory
+                .select(dailyAnalysis.strategy.strategyId, dailyAnalysis.dailyDate,
+                        dailyAnalysis.cumulativeProfitLossRate)
+                .from(dailyAnalysis)
+                .fetch()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        tuple -> tuple.get(dailyAnalysis.strategy.strategyId)
+                ));
+    }
+
+    @Override
+    public Map<Long, List<String>> findStockTypeIconsMap(List<Long> strategyIdS) {
+        // 종목 아이콘 조회
+        List<Tuple> stockTypeIcons = queryFactory
+                .select(stockTypeGroup.strategy.strategyId, stockType.stockTypeIconURL)
+                .from(stockTypeGroup)
+                .join(stockTypeGroup.stockType, stockType)
+                .fetch();
+
+        Map<Long, List<String>> stockTypeIconUrlsMap = new HashMap<>();
+
+        for (Tuple stockTypeIcon : stockTypeIcons) {
+            Long strategyId = stockTypeIcon.get(stockTypeGroup.strategy.strategyId);
+            String iconUrl = stockTypeIcon.get(stockType.stockTypeIconURL);
+            stockTypeIconUrlsMap
+                    .computeIfAbsent(strategyId, key -> new ArrayList<>()) // 전략 id가 없으면 빈 리스트 생성
+                    .add(iconUrl);
+        }
+        return stockTypeIconUrlsMap;
+    }
+
+    @Override
+    public Map<Long, Boolean> findBySubscriptionMap(Long userId,List<Long> strategyIdS) {
+        // 구독 여부 조회
+        return queryFactory
+                .select(subscription.strategy.strategyId, subscription.user.userId)
+                .from(subscription)
+                .where(subscription.user.userId.eq(userId))
+                .fetch()
+                .stream().collect(Collectors.toMap(
+                        tuple -> tuple.get(subscription.strategy.strategyId),
+                        tuple -> true
+                ));
+    }
+
+    private BooleanBuilder applyAllFilters(FilterSearchRequest filterSearchRequest) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // 조건 추가
+        builder.and(applySearchWordFilter(filterSearchRequest.getSearchWord()));
+        builder.and(applyTradeTypeFilter(filterSearchRequest.getTradeType()));
+        builder.and(applyOperationCycleFilter(filterSearchRequest.getOperationCycle()));
+        builder.and(applyStockTypeFilter(filterSearchRequest.getStockTypes()));
+        builder.and(applyOperationPeriodFilter(filterSearchRequest.getDuration()));
+        builder.and(applyProfitRangeFilter(filterSearchRequest.getProfitRange()));
+        builder.and(applyPrincipalRangeFilter(filterSearchRequest.getPrincipalRange()));
+        builder.and(applyMddRangeFilter(filterSearchRequest.getMddRange()));
+        builder.and(applySmScoreRangeFilter(filterSearchRequest.getSmScoreRange()));
+
+        return builder;
+    }
+
+
+    // 전략명 검색어 필터
+    private BooleanExpression applySearchWordFilter(String searchWord) {
+        return searchWord == null ? null : strategy.strategyName.like("%" + searchWord + "%");
+    }
+
+    // 운용방식(매매유형) 필터
+    private BooleanExpression applyTradeTypeFilter(List<String> tradeTypeNames) {
+        return tradeTypeNames == null ? null : tradeType.tradeTypeName.in(tradeTypeNames);
+    }
+
+    // 운용주기 필터 (데이/포지션)
+    private BooleanExpression applyOperationCycleFilter(List<OperationCycle> operationCycles) {
+        return operationCycles == null ? null : strategy.operationCycle.in(operationCycles);
+    }
+
+    // 운용종목 필터
+    private BooleanExpression applyStockTypeFilter(List<String> stockTypeNames) {
+        return stockTypeNames == null ? null : stockType.stockTypeName.in(stockTypeNames);
+    }
+
+    // 운용기간 필터
+    private BooleanExpression applyOperationPeriodFilter(List<DurationRange> durationRanges) {
+        if (durationRanges == null || durationRanges.isEmpty()) {
+            return null;
+        }
+
+        BooleanExpression condition = null;
+        for (DurationRange range : durationRanges) {
+            BooleanExpression rangeCondition = strategyStatistics.operationPeriod.between(range.getMinDays(),
+                    range.getMaxDays());
+            condition = (condition == null) ? rangeCondition : condition.or(rangeCondition);
+        }
+
+        return condition;
+    }
+
+    // 수익률 필터
+    private BooleanExpression applyProfitRangeFilter(List<ProfitRange> profitRanges) {
+        if (profitRanges == null || profitRanges.isEmpty()) {
+            return null;
+        }
+
+        BooleanExpression condition = null;
+        for (ProfitRange range : profitRanges) {
+            BooleanExpression rangeCondition = strategyStatistics.operationPeriod.between(range.getMinRate(),
+                    range.getMaxRate());
+            condition = (condition == null) ? rangeCondition : condition.or(rangeCondition);
+        }
+
+        return condition;
+
+    }
+
+    // 원금 필터
+    private BooleanExpression applyPrincipalRangeFilter(RangeDto principalRange) {
+        return principalRange == null ? null
+                : strategyStatistics.principal.between(principalRange.getMin(), principalRange.getMax());
+    }
+
+    // MDD 필터
+    private BooleanExpression applyMddRangeFilter(RangeDto mddRange) {
+        return mddRange == null ? null
+                : strategyStatistics.maxDrawdown.between(mddRange.getMin(), mddRange.getMax());
+    }
+
+    // SM Score 필터
+    private BooleanExpression applySmScoreRangeFilter(RangeDto smScoreRange) {
+        return smScoreRange == null ? null
+                : strategyStatistics.smScore.between(smScoreRange.getMin(), smScoreRange.getMax());
     }
 
 }
