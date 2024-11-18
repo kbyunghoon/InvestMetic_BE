26e54f9c7788d0dd53b4e7418e2a6e7572e02f32
5jeong <wjdgns4385@naver.com>
2024-11-18 21:16:19 +0900

    ✨ [feat] 전략 리뷰수 기능추가

--- src/main/java/com/investmetic/domain/review/dto/response/ReviewListResponse.java
+++ src/main/java/com/investmetic/domain/review/dto/response/ReviewListResponse.java
@@ -9,12 +9,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReviewListResponse {
     private final double averageRating;
+    private final int reviewCount;
     private final PageResponseDto<ReviewDetailResponse> reviews;
 
     // 정적 팩토리 메서드
-    public static ReviewListResponse createReviewListResponse(double averageRating,
+    public static ReviewListResponse createReviewListResponse(double averageRating,int reviewCount,
                                                               PageResponseDto<ReviewDetailResponse> reviews) {
-        return new ReviewListResponse(RoundUtil.roundToSecond(averageRating), reviews);
+        return new ReviewListResponse(RoundUtil.roundToSecond(averageRating), reviewCount,reviews);
     }
 
 }
