fdbb9cd72622374f68714cbf1fe58bbd0208acb9
5jeong <wjdgns4385@naver.com>
2024-11-20 15:54:37 +0900

    ✨ [feat] 전략 랭킹페이지 진입시 요청 기능추가

--- src/main/java/com/investmetic/domain/strategy/controller/StrategyRankingController.java
+++ src/main/java/com/investmetic/domain/strategy/controller/StrategyRankingController.java
@@ -2,7 +2,9 @@ package com.investmetic.domain.strategy.controller;
 
 import com.investmetic.domain.strategy.dto.request.AlgorithmSearchRequest;
 import com.investmetic.domain.strategy.dto.request.FilterSearchRequest;
+import com.investmetic.domain.strategy.dto.response.RegisterInfoResponseDto;
 import com.investmetic.domain.strategy.dto.response.common.StrategySimpleResponse;
+import com.investmetic.domain.strategy.service.StrategyRegisterService;
 import com.investmetic.domain.strategy.service.StrategySearchService;
 import com.investmetic.global.common.PageResponseDto;
 import com.investmetic.global.exception.BaseResponse;
@@ -11,8 +13,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,10 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 //TODO : 스프링시큐리티 적용시 리팩토링
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/strategies/search")
 public class StrategyRankingController {
     private final StrategySearchService strategySearchService;
+    private final StrategyRegisterService strategyRegisterService;
 
-    @PostMapping("/api/strategies/search/filters")
+    @PostMapping("/filters")
     public ResponseEntity<BaseResponse<PageResponseDto<StrategySimpleResponse>>> searchByFilters(
             @RequestBody FilterSearchRequest filterSearchRequest,
             @RequestParam Long userId,
@@ -34,7 +40,7 @@ public class StrategyRankingController {
         return BaseResponse.success(result);
     }
 
-    @PostMapping("/api/strategies/search/algorithm")
+    @PostMapping("/algorithm")
     public ResponseEntity<BaseResponse<PageResponseDto<StrategySimpleResponse>>> searchByAlgorithm(
             @RequestBody AlgorithmSearchRequest algorithmSearchRequest,
             @RequestParam Long userId,
@@ -45,5 +51,10 @@ public class StrategyRankingController {
         return BaseResponse.success(result);
     }
 
+    // 랭킹 페이지 진입시 요청
+    @GetMapping
+    public ResponseEntity<BaseResponse<RegisterInfoResponseDto>> loadStrategyRegistrationInfo() {
+        return BaseResponse.success(strategyRegisterService.loadStrategyRegistrationInfo());
+    }
 
 }
