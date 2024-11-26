2bcb8535305893523fb738b2650c7974e921a490
5jeong <wjdgns4385@naver.com>
2024-11-26 18:04:10 +0900

    🐛 [fix] 테스트코드 영속성 컨텍스트 문제 해결

--- src/test/java/com/investmetic/domain/strategy/service/MyDailyAnalysisServiceTest.java
+++ src/test/java/com/investmetic/domain/strategy/service/MyDailyAnalysisServiceTest.java
@@ -9,8 +9,12 @@ import com.investmetic.domain.strategy.model.entity.Proceed;
 import com.investmetic.domain.strategy.model.entity.Strategy;
 import com.investmetic.domain.strategy.model.entity.TradeType;
 import com.investmetic.domain.strategy.repository.DailyAnalysisRepository;
+import com.investmetic.domain.strategy.repository.StrategyRepository;
+import com.investmetic.domain.strategy.repository.TradeTypeRepository;
 import com.investmetic.domain.user.model.entity.User;
+import com.investmetic.domain.user.repository.UserRepository;
 import com.investmetic.global.common.PageResponseDto;
+import jakarta.persistence.EntityManager;
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
@@ -32,15 +36,34 @@ class MyDailyAnalysisServiceTest {
     private StrategyAnalysisService strategyAnalysisService;
 
     @Autowired
+    private StrategyRepository strategyRepository;
+
+    @Autowired
+    private TradeTypeRepository tradeTypeRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private DailyAnalysisRepository dailyAnalysisRepository;
+
+    @Autowired
+    private EntityManager em;
 
     private Strategy strategy;
 
     @BeforeEach
     void setUp() {
         User user = TestEntityFactory.createTestUser();
+        userRepository.save(user);
         TradeType tradeType = TestEntityFactory.createTestTradeType();
+        tradeTypeRepository.save(tradeType);
         strategy = TestEntityFactory.createTestStrategy(user, tradeType);
+
+        // 전략 저장 및 영속화
+        strategyRepository.save(strategy);
+        em.flush(); // 데이터베이스에 반영
+        em.clear(); // 영속성 컨텍스트 초기화
     }
 
     @Test
@@ -65,6 +88,9 @@ class MyDailyAnalysisServiceTest {
 
         dailyAnalysisRepository.save(dailyAnalysis1);
         dailyAnalysisRepository.save(dailyAnalysis2);
+
+        em.flush();
+        em.clear();
 
         Pageable pageable = PageRequest.of(0, 10);
 
@@ -103,6 +129,9 @@ class MyDailyAnalysisServiceTest {
 
         dailyAnalysisRepository.save(dailyAnalysis1);
         dailyAnalysisRepository.save(dailyAnalysis2);
+
+        em.flush();
+        em.clear();
 
         Pageable pageable = PageRequest.of(0, 10);
 
