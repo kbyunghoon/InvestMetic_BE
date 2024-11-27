9df3d3b21af21494d25c47f981258cdb9f5b4165
zjall0020 <zjall0020@gmail.com>
2024-11-27 13:38:05 +0900

    [test] 문의 삭제 테스트 코드 작성

--- src/test/java/com/investmetic/domain/qna/service/QuestionServiceTest.java
+++ src/test/java/com/investmetic/domain/qna/service/QuestionServiceTest.java
@@ -11,6 +11,7 @@ import com.investmetic.domain.qna.repository.QuestionRepository;
 import com.investmetic.domain.strategy.model.entity.Strategy;
 import com.investmetic.domain.strategy.model.entity.TradeType;
 import com.investmetic.domain.strategy.repository.StrategyRepository;
+import com.investmetic.domain.user.model.Role;
 import com.investmetic.domain.user.model.entity.User;
 import com.investmetic.domain.user.repository.UserRepository;
 import java.util.Optional;
@@ -18,6 +19,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 class QuestionServiceTest {
@@ -63,6 +65,47 @@ class QuestionServiceTest {
         verify(questionRepository).save(any(Question.class));
     }
 
+    @Test
+    @DisplayName("문의 삭제 성공")
+    void deleteQuestion_Success() {
+        // Given
+        Long strategyId = 1L;
+        Long questionId = 1L;
+        Long userId = 1L;
 
+        // 사용자(Mock User) 생성
+        User mockUser = Mockito.mock(User.class);
+        when(mockUser.getUserId()).thenReturn(userId);
+        when(mockUser.getRole()).thenReturn(Role.INVESTOR);
+        when(mockUser.getNickname()).thenReturn("TestUser");
+
+
+        // 전략(Mock Strategy) 생성 (사용자 설정 필수)
+        Strategy mockStrategy = Strategy.builder()
+                .strategyId(strategyId)
+                .user(mockUser) // 전략의 사용자 설정
+                .build();
+
+        // 문의(Mock Question) 생성
+        QuestionRequestDto questionRequestDto = QuestionRequestDto.builder()
+                .title("문의 제목")
+                .content("내용 ㅋㅋ")
+                .build();
+        Question mockQuestion = Question.from(mockUser, mockStrategy,questionRequestDto);
+
+        // Mock 설정
+        when(userRepository.findById(userId)).thenReturn(Optional.of(mockUser));
+        when(strategyRepository.findById(strategyId)).thenReturn(Optional.of(mockStrategy));
+        when(questionRepository.findById(questionId)).thenReturn(Optional.of(mockQuestion));
+
+        // When
+        questionService.deleteQuestion(strategyId, questionId, userId);
+
+        // Then
+        verify(userRepository).findById(userId); // 사용자 조회 검증
+        verify(strategyRepository).findById(strategyId); // 전략 조회 검증
+        verify(questionRepository).findById(questionId); // 문의 조회 검증
+        verify(questionRepository).delete(mockQuestion); // 삭제 호출 검증
+    }
 
 }
