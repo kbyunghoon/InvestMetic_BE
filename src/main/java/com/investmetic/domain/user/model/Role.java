9c1a4a61ccb2feb13cf05952b24e4927000751d5
woong <153367605+kendrickhama@users.noreply.github.com>
2024-11-27 15:35:57 +0900

    ♻️ [refactor] Role enum "ROLE_OOO" 로 수정

--- src/main/java/com/investmetic/domain/user/model/Role.java
+++ src/main/java/com/investmetic/domain/user/model/Role.java
@@ -1,12 +1,17 @@
 package com.investmetic.domain.user.model;
 
 public enum Role {
-    TRADER,         // 트레이더
-    INVESTOR,       // 투자자
-    TRADER_ADMIN,   // 트레이더 관리자
-    INVESTOR_ADMIN,  // 투자자 관리자
-    SUPER_ADMIN;
+    TRADER("ROLE_TRADER"),         // 트레이더
+    INVESTOR("ROLE_INVESTOR"),     // 투자자
+    TRADER_ADMIN("ROLE_TRADER_ADMIN"),   // 트레이더 관리자
+    INVESTOR_ADMIN("ROLE_INVESTOR_ADMIN"), // 투자자 관리자
+    SUPER_ADMIN("ROLE_SUPER_ADMIN");     // 슈퍼 관리자
 
+    private final String role;
+
+    Role(String role) {
+        this.role = role;
+    }
     // admin일 경우 true;
     public static boolean isAdmin(Role role) {
         return switch (role) {
