b4320273881bdb4737ef7593bfdde8ac20e625fa
woong <153367605+kendrickhama@users.noreply.github.com>
2024-11-27 15:33:28 +0900

    ♻️ [refactor] 사용 안 하는 메서드 제거

--- src/main/java/com/investmetic/domain/user/dto/response/CustomUserDetails.java
+++ src/main/java/com/investmetic/domain/user/dto/response/CustomUserDetails.java
@@ -3,17 +3,15 @@ package com.investmetic.domain.user.dto.response;
 import com.investmetic.domain.user.model.entity.User;
 import java.util.Collection;
 import java.util.Collections;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+@RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
 
     private final User user;
-
-    public CustomUserDetails(User user) {
-        this.user = user;
-    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -33,25 +31,5 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public String getUsername() {
         return user.getEmail();     //이메일을 고유식별자로 사용
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 }
