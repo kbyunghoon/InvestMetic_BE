1fcd5b398dba9a1639345051f733a16c631fe119
zjall0020 <zjall0020@gmail.com>
2024-11-28 16:36:14 +0900

    ♻️ [refactor] 빌드가 안되서 dto에 @Builder 어노테이션 삭제

--- src/main/java/com/investmetic/domain/qna/dto/request/QuestionRequestDto.java
+++ src/main/java/com/investmetic/domain/qna/dto/request/QuestionRequestDto.java
@@ -3,7 +3,6 @@ package com.investmetic.domain.qna.dto.request;
 import com.investmetic.domain.qna.dto.SearchCondition;
 import com.investmetic.domain.qna.dto.StateCondition;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -18,7 +17,6 @@ public class QuestionRequestDto {
     @NotBlank(message = "문의 내용을 입력해주세요.")
     private final String content;
 
-    @Builder
     public QuestionRequestDto(String keyword, SearchCondition searchCondition, StateCondition stateCondition,
                               String title, String content) {
         this.keyword = keyword;
