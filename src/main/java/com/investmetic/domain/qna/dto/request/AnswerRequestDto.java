b3af96584f1a82473681104ae79f78dc5b24916e
zjall0020 <zjall0020@gmail.com>
2024-11-28 15:27:32 +0900

    ♻️ [refactor] @NoArgsConstructor 삭제 후 코드 정리, message 문의 -> 답변 으로 수정

--- src/main/java/com/investmetic/domain/qna/dto/request/AnswerRequestDto.java
+++ src/main/java/com/investmetic/domain/qna/dto/request/AnswerRequestDto.java
@@ -3,16 +3,14 @@ package com.investmetic.domain.qna.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class AnswerRequestDto {
-    @NotBlank(message = "문의 내용을 입력해주세요.")
+    @NotBlank(message = "답변 내용을 입력해주세요.")
     private String content;
 
     @Builder
-    public AnswerRequestDto(String content){
+    public AnswerRequestDto(String content) {
         this.content = content;
     }
 }
