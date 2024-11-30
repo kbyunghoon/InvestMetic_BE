2500944deef92acf0a4186dd4b205023c2e62768
RG <jlwoo092513@gmail.com>
2024-11-30 13:33:48 +0900

    :memo: [docs] : LoginController swagger 볼수 있도록 추가.
    프론트의 요청으로 LoginAPI를 Swagger에서 볼 수 있도록 하였습니다.

---
+++ src/main/java/com/investmetic/domain/user/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.investmetic.domain.user.controller;
+
+
+import com.investmetic.domain.user.dto.request.PasswordDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "로그인 API")
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @Operation(summary = "로그인 기능",
+            description = "<a href='https://www.notion.so/789758335eef4feb879460367a16cf90' target='_blank'>API 명세서</a>")
+    @PostMapping
+    public void login(@RequestBody PasswordDto passwordDto){
+
+        // 여기까지 안들어옴 그전에 필터에서 반환. 보여주기용.
+    }
+
+}
