08a8e576814c29c2e68e798abf9e9df23568bdd9
ghkdwjdgk123 <ghkdwjdgk123@gmail.com>
2024-12-03 15:58:15 +0900

    [refacor] 시큐리티 인가 설정 적용

--- src/main/java/com/investmetic/domain/subscription/controller/SubscribeController.java
+++ src/main/java/com/investmetic/domain/subscription/controller/SubscribeController.java
@@ -2,10 +2,13 @@ package com.investmetic.domain.subscription.controller;
 
 import com.investmetic.domain.subscription.service.SubscriptionService;
 import com.investmetic.global.exception.BaseResponse;
+import com.investmetic.global.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +24,12 @@ public class SubscribeController {
     @Operation(summary = "전략 구독, 구독 취소 상태 관리 기능",
             description = "<a href='https://www.notion.so/dfae7b994dd94761a77f70869bf9d479' target='_blank'>API 명세서</a>")
     @GetMapping("/{strategyId}/subscribe")
+    @PreAuthorize("hasRole('ROLE_TRADER') or hasRole('ROLE_INVESTOR')")
     public ResponseEntity<BaseResponse<Void>> subscribe(
             @PathVariable Long strategyId,
-            @RequestParam Long userId
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
     ) {
-        subscriptionService.subscribe(strategyId, userId);
+        subscriptionService.subscribe(strategyId, customUserDetails.getUserId());
         return BaseResponse.success();
     }
 }
