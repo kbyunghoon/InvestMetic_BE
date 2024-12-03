4ab87d6619b68ef253bc4c31d90fd3c6ed59a653
5jeong <wjdgns4385@naver.com>
2024-12-03 16:06:27 +0900

    ♻️ [refactor] 주석추가

--- src/test/java/com/investmetic/domain/subscription/service/SubscribtionServiceTest.java
+++ src/test/java/com/investmetic/domain/subscription/service/SubscribtionServiceTest.java
@@ -1,6 +1,7 @@
 package com.investmetic.domain.subscription.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
 
 import com.investmetic.domain.strategy.model.IsPublic;
 import com.investmetic.domain.strategy.model.entity.Strategy;
@@ -12,10 +13,13 @@ import com.investmetic.domain.subscription.repository.SubscriptionRepository;
 import com.investmetic.domain.user.model.Role;
 import com.investmetic.domain.user.model.entity.User;
 import com.investmetic.domain.user.repository.UserRepository;
+import com.investmetic.global.exception.BusinessException;
+import com.investmetic.global.exception.ErrorCode;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -87,6 +91,26 @@ public class SubscribtionServiceTest {
         subscriptionCount = strategyRepository.findById(strategyId).get().getSubscriptionCount();
         assertThat(subscriptionCount).isEqualTo(0);
 
+    }
+
+    @DisplayName("본인전략의 구독할때 예외테스트")
+    @Test
+    public void 구독_예외_테스트() {
+
+        // Given
+        Strategy strategy = strategyRepository.findAll().get(0); // 첫 번째 전략
+        Long strategyId = strategy.getStrategyId();
+        Long userId = strategy.getUser().getUserId(); // 전략 소유자 ID
+
+        // When & Then
+        BusinessException exception = assertThrows(
+                BusinessException.class,
+                () -> subscriptionService.subscribe(strategyId, userId) // 본인의 전략을 구독 시도
+        );
+
+        // 예외 검증
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.SELF_SUBSCRIPTION_NOT_ALLOWED);
+        assertThat(exception.getMessage()).isEqualTo("본인 전략에는 구독할 수 없습니다.");
     }
 
 }
