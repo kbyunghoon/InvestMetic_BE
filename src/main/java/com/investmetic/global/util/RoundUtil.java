4a1b95992dabf6abff366d01dce1f74d39f5c8db
5jeong <wjdgns4385@naver.com>
2025-04-29 16:24:24 +0900

    🐛 [fix] RoundUtil 유효성 검사 추가

--- src/main/java/com/investmetic/global/util/RoundUtil.java
+++ src/main/java/com/investmetic/global/util/RoundUtil.java
@@ -8,34 +8,30 @@ import java.math.RoundingMode;
  */
 public class RoundUtil {
 
-    // 소수점 첫째 자리에서 반올림
-    public static double roundToFirst(double value) {
-        return round(value, 0);
+
+    public static double roundToSecond(Double value) {
+        return safeRound(value, 1);
     }
 
-    // 소수점 둘째 자리에서 반올림
-    public static double roundToSecond(double value) {
-        return round(value, 1);
+    public static double roundToFifth(Double value) {
+        return safeRound(value, 4);
     }
 
-    // 소수점 셋째 자리에서 반올림
-    public static double roundToThird(double value) {
-        return round(value, 2);
-    }
+    /**
+     * 안전하게 반올림 수행 (null, NaN, Infinity 방어)
+     */
+    private static double safeRound(Double value, int scale) {
+        if (value == null || !Double.isFinite(value)) {
+            return 0.0;
+        }
 
-    // 소수점 넷째 자리에서 반올림
-    public static double roundToFourth(double value) {
-        return round(value, 3);
-    }
-
-    // 소수점 다섯째 자리에서 반올림
-    public static double roundToFifth(double value) {
-        return round(value, 4);
-    }
-
-    private static double round(double value, int idx) {
-        BigDecimal bigDecimal = BigDecimal.valueOf(value);
-        return bigDecimal.setScale(idx, RoundingMode.HALF_UP).doubleValue();
+        try {
+            return BigDecimal.valueOf(value)
+                    .setScale(scale, RoundingMode.HALF_UP)
+                    .doubleValue();
+        } catch (NumberFormatException e) {
+            return 0.0;
+        }
     }
 
 }
